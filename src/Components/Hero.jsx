8dfb8408d99b52eefe6944449e98025c3692e700
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-03-18 22:53:51 +0530

    for desktop screen hero section is done

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,32 +4,33 @@ import magentaWheel  from '../assets/magenta.png'
 
 const Hero = () => {
   return (
-   <div className='bg-linear-to-r relative h-[38vw] from-[#3A99C9] to-[#0073AE]'>
+   <div className='bg-linear-to-r relative h-[40vw] from-[#3A99C9] to-[#0073AE]'>
         <div>
           <img src={magentaWheel} className='absolute h-[17vw] top-[50%] z-50 left-[50%] translate-[-50%]'  alt="" />
-           <h1 className='absolute top-[50%] text-[1.5vw] font-bold text-white w-[12vw] text-center  left-[50%] translate-[-50%]'>The HR Company</h1>
-       </div>
-
-       <div>
-          <img src={blueWheel} className='absolute h-[17vw] top-[34%] right-[11%] translate-[-50%]'  alt="" />
-           <h1 className='absolute top-[30%] left-[32%] translate-[-50%] text-[1.5vw] font-bold text-white w-[11vw] text-center'> Leadership Coaching & Development</h1>
+           <h1 className='absolute top-[50%] text-[1.8vw] font-[500] text-white w-[10vw] text-center  left-[50%] translate-[-50%]'>The HR <br /> Company</h1>
        </div>
 
        <div>
           <img src={blueWheel} className='absolute h-[17vw] top-[32%] left-[33.5%] translate-[-50%]'  alt="" />
-           <h1 className='absolute top-[30%] left-[32%] translate-[-50%] text-[1.5vw] font-bold text-white w-[11vw] text-center'> Leadership Coaching & Development</h1>
+           <h1 className='absolute top-[32%] left-[33.5%] translate-[-50%] text-[1.5vw] font-[500] text-white w-[11vw] text-center'> Leadership Coaching & Development</h1>
        </div>
 
 
        <div>
+          <img src={blueWheel} className='absolute h-[17vw] top-[34%] right-[11%] translate-[-50%]'  alt="" />
+           <h1 className='absolute top-[34%] right-[19%] translate-[-50%] text-[1.5vw] font-[500] text-white w-[11vw] text-center'> Empowered performance</h1>
+       </div>
+
+      
+       <div>
           <img src={blueWheel} className='absolute h-[14vw] bottom-[-14.9%] left-[37.5%] translate-[-50%]'  alt="" />
-           <h1 className='absolute top-[30%] left-[32%] translate-[-50%] text-[1.5vw] font-bold text-white w-[11vw] text-center'> Leadership Coaching & Development</h1>
+           <h1 className='absolute bottom-[2vw] left-[37.5%] translate-[-50%] text-[1.3vw] font-[500] text-white w-[10vw] text-center'>Advisory & People Processes</h1>
        </div>
          
 
        <div>
           <img src={blueWheel} className='absolute h-[14vw] bottom-[-13%] right-[19%] translate-[-50%]'  alt="" />
-           <h1 className='absolute top-[30%] left-[32%] translate-[-50%] text-[1.5vw] font-bold text-white w-[11vw] text-center'> Leadership Coaching & Development</h1>
+           <h1 className='absolute bottom-[12%] right-[26.5%]  translate-[-50%] text-[1.5vw] font-[500] text-white w-[8vw] text-center'> Team Solutions</h1>
        </div>
          
    </div>
