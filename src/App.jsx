90cbf8fae8e427b70252340d7a2717a86535db77
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-03-18 23:38:56 +0530

    responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,27 @@
 import React from 'react'
 import Home from './Pages/Home'
 import leftRibbin from './assets/left-ribbin.png';
-import logo from './assets/logo.png'
+import Navbar from './Components/Navbar';
 const App = () => {
   return (
       <div>
-       
-
-          {/* nav bar  */}
-          <header className="bg-white max-w-[98%] m-auto flex justify-between items-center">
-            <img src={logo} className='h-[6vw]' alt="" />
-            <nav>
-              <ul className="flex gap-10 text-lg  text-gray-600">
-                <li>Home</li>
-                <li>About</li>
-                <li>Why Us</li>
-                <li>Solutions</li>
-                <li>Blogs</li>
-                <li>Contact Us</li>
-              </ul>
-            </nav>
-          </header>
+           <Navbar/>
 
 
-      <div  className='flex gap-2.5 sm:gap-20'>
-        {/* left ribbin  */}
-          <div className='w-[8vw]'>
-             <img src={leftRibbin} alt="Left Ribbon" />
-          </div>
+            <div  className='flex gap-2.5 sm:gap-20'>
+              {/* left ribbin  */}
+                <div className='w-[8vw]'>
+                  <img src={leftRibbin} alt="Left Ribbon" />
+                </div>
 
 
-          {/* right content  */}
-          <div className='w-[85%]'>
-            <Home/>
-          </div>
+              {/* right content  */}
+              <div className='w-[85%]'>
+                <Home/>
+              </div>
+          
       
-      
-      </div>
+            </div>
 
     </div>  
   )
