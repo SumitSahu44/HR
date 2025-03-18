f4cba1613d88f1bcd7ce69164e9d563ecf2f80eb
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-03-18 23:59:52 +0530

    contact component

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='bg-linear-to-r mt-[2vw] flex justify-center items-center w-[100%] h-[10vw] from-[#3A99C9]  to-[#0073AE]'>
+         <div className='flex justify-between w-[90%] m-auto text-white items-center'>
+              <div>
+                <h2 className='text-3xl'>www.thehrcompany.co.in</h2>
+                <h2 className='text-3xl'>Gurgaon & Mumbai</h2>
+              </div>
+
+              <div>
+                  <p className='text-2xl'>+91-9930951967/ + 91-2225797562</p>
+                  <p className='text-2xl'>+91-9930951967/ + 91-2225797562</p>
+                  <p className='text-2xl'>https://www.linkedin.com/the-hr-company/</p>
+              </div>
+         </div>
+    </div>
+  )
+}
+
+export default Contact
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Hero from '../Components/Hero'
+import Contact from '../Components/Contact'
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <Contact/>
     </div>
   )
 }
