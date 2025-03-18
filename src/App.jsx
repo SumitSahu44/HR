cca6280dc4f084ce67807a8b6aca9bb6fcd9d0ec
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-03-18 19:21:12 +0530

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import Home from './Pages/Home'
 import leftRibbin from './assets/left-ribbin.png';
+import logo from './assets/logo.png'
 const App = () => {
   return (
-      <div className='flex'>
+      <div>
+       
+
+          {/* nav bar  */}
+          <header className="bg-white max-w-[98%] m-auto flex justify-between items-center">
+            <img src={logo} className='h-[6vw]' alt="" />
+            <nav>
+              <ul className="flex gap-10 text-lg  text-gray-600">
+                <li>Home</li>
+                <li>About</li>
+                <li>Why Us</li>
+                <li>Solutions</li>
+                <li>Blogs</li>
+                <li>Contact Us</li>
+              </ul>
+            </nav>
+          </header>
+
+
+      <div  className='flex gap-20'>
         {/* left ribbin  */}
-          <div>
-          <img src={leftRibbin} className='w-full h-[100vh]' alt="Left Ribbon" />
-          
+          <div className='w-[8vw]'>
+             <img src={leftRibbin} alt="Left Ribbon" />
           </div>
 
 
@@ -18,6 +37,8 @@ const App = () => {
       
       
       </div>
+
+    </div>  
   )
 }
 
