7389cac1552bcb81320455bd067879f0703246b3
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-03-21 22:39:45 +0530

    basic heading section with line reated

--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 const Heading = () => {
   return (
-    <div className='mt-[2vw]'>
+    <div className='mt-[2vw] relative'>
 
-      <h2 className='bg-[#006699]  ml-[5vw] text-[2.5vw] pl-[4vw] text-white'>Our Strength </h2>
-     
+    <div className='relative'>
+        <h2 className='bg-[#006699]   ml-[5vw]  text-[2.5vw] pl-[4vw] text-white'>Our Strength </h2>
+        
+        <div className='w-[50vw] absolute h-[0.5vw] top-[5vw] bg-amber-800'></div>
+        <div className='w-[0.5vw]  absolute top-[0vw] left-[3vw] h-[25vw] bg-amber-800'></div>
+    
+      
+    </div>
+  
     </div>
   )
 }
