930a690d749be5a8675572980b632cc6769fdb60
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-03-21 22:54:03 +0530

    add content

--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -4,14 +4,26 @@ const Heading = () => {
   return (
     <div className='mt-[2vw] relative'>
 
-    <div className='relative'>
+
         <h2 className='bg-[#006699]   ml-[5vw]  text-[2.5vw] pl-[4vw] text-white'>Our Strength </h2>
         
         <div className='w-[50vw] absolute h-[0.5vw] top-[5vw] bg-amber-800'></div>
-        <div className='w-[0.5vw]  absolute top-[0vw] left-[3vw] h-[25vw] bg-amber-800'></div>
+        <div className='w-[0.5vw]  absolute top-[0vw] left-[3vw] h-full bg-amber-800'></div>
     
+
+        <div className='flex ml-[5vw] mt-[3vw] gap-10 min-w-[300px] flex-wrap justify-between'>
+            <div className='bg-[#3A99C9] p-[30px]'>
+               <p>Shaping and building Talent, performance, teams and leader</p>
+            </div>
+            <div className='bg-[#3A99C9] p-[30px]'>
+               <p>Shaping and building Talent, performance, teams and leader</p>
+            </div>
+            <div className='bg-[#3A99C9] p-[30px]'>
+               <p>Shaping and building Talent, performance, teams and leader</p>
+            </div>
+         </div>
       
-    </div>
+
   
     </div>
   )
