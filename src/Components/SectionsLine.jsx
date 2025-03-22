cc29a7ea77154806bf05143f2d8eaa8fb659710e
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-03-22 23:46:09 +0530

    woeking with all section and fix w-ful to '

--- src/Components/SectionsLine.jsx
+++ src/Components/SectionsLine.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SectionsLine = () => {
   return (
-    <div className='bg-[#969696] w-full ml-[3vw] h-[4px] mt-[60px] mb-[60px]'>
+    <div className='bg-[#969696]  ml-[3vw] h-[4px] mt-[60px] mb-[60px]'>
       
     </div>
   )
