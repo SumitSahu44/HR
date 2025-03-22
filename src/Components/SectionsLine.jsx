6c6a24484be8c1703cc84b806237cd5af1dc97de
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-03-22 23:37:08 +0530

    sectionLine

---
+++ src/Components/SectionsLine.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SectionsLine = () => {
+  return (
+    <div className='bg-[#969696] w-full ml-[3vw] h-[4px] mt-[60px] mb-[60px]'>
+      
+    </div>
+  )
+}
+
+export default SectionsLine
