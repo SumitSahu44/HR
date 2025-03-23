69cdd9a8cfa2062f96bf73cf46f94445e5ff3589
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-03-23 23:20:37 +0530

    icons location setup

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import CommonHeading from './CommonHeading';
+import { IoMdSettings } from "react-icons/io";
+
 const cardData = [
   {
     title: 'Empowered Performance',
@@ -25,9 +27,14 @@ const cardData = [
 
 const Card = ({ title, description }) => (
   <div className="flex ml-[5vw] mt-[20px]  items-start border-l-6 border-[#969696] pl-4 p-6  bg-white">
-    <img alt="Pink gears icon" className="mr-4 mt-1" src="https://placehold.co/30x30" />
+    
     <div>
-      <h2 className="text-xl font-bold mb-2">{title}</h2>
+        <div className='relative'>
+           <IoMdSettings className='w-[30px] absolute top-[-15px] left-[24px] h-[30px]  text-pink-500'/>
+           <IoMdSettings className='w-[30px]  h-[30px]  text-pink-500'/>
+       </div>
+  
+      <h2 className="text-xl  font-bold mb-2">{title}</h2>
       <p className="text-gray-600 mb-4">{description}</p>
       <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
         Read More
@@ -41,6 +48,7 @@ const Services = () => {
   <div className='mt-[2vw] relative'>
      <CommonHeading title="Services Architecture"/>
     <div className="container mx-auto px-4 py-8">
+        
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         {cardData.map((item, index) => (
           <Card key={index} title={item.title} description={item.description} />
