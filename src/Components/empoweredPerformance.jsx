2eeb3ceb55e02a770006385e8e307356fa16a974
RamLakhan825 <rljangid825@gmail.com>
2025-03-24 18:21:43 +0530

    add responsiveness

--- src/Components/empoweredPerformance.jsx
+++ src/Components/empoweredPerformance.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import CommonHeading from "./CommonHeading";
-import image2 from "../assets/image 2.png"; 
+import image2 from "../assets/image 2.png";
 
 const EmpoweredPerformance = () => {
   return (
-    <div className="mt-10 relative px-16 lg:px-32 xl:px-48">
+    <div className="mt-[2vw] relative">
       <CommonHeading title="Empowered Performance" />
 
-      <div className="flex justify-center mt-12">
+      <div className="flex justify-center mt-8 sm:mt-10 md:mt-12">
         <img
           src={image2}
           alt="Empowered Performance"
-          className="w-full max-w-[70%] lg:max-w-3xl xl:max-w-5xl shadow-xl rounded-lg"
+          className="w-full max-w-[95%] sm:max-w-[85%] md:max-w-2xl lg:max-w-3xl xl:max-w-4xl 2xl:max-w-5xl shadow-xl rounded-lg relative left-[5vw]"
         />
       </div>
     </div>
